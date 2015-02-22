cdca95ac8c388f3eca6ac6afbe08219f708b4487
Gaurav Kanthed <kanthedgaurav@yahoo.com>
2015-02-22 15:02:30 -0500

    Coursera Assignment of R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,19 @@
 #Caching the Inverse of a Matrix Program
 
 # makeCacheMatrix function creates a special "matrix" object that can cache its inverse
-#set the value of the matrix
-#get the value of the matrix
-#set the value of the inverse of matrix
-#get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   iom <- NULL
+  #set the value of the matrix
   set <- function(y) {
     x <<- y
     iom <<- NULL
   }
+  #get the value of the matrix
   get <- function() x
+  #set the value of the inverse of matrix
   setinverseofmatrix <- function(inverseofmatrix) iom <<- inverseofmatrix
+  #get the value of the inverse of matrix
   getinverseofmatrix <- function() iom
   list(set = set, get = get,
        setinverseofmatrix = setinverseofmatrix,
@@ -21,17 +21,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+# cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  #get the inverse of matrix 
   iom <- x$getinverseofmatrix()
+  #checking if cached data available
   if(!is.null(iom)) {
     message("getting cached data")
     return(iom)
   }
+  #stroe matrix into data variable
   data <- x$get()
-  iom <- solve(data)
+  #Computing the inverse of a square matrix using the solve function in R
+  iom <- solve(data,...)
+  #set the iverse of matrix 
   x$setinverseofmatrix(iom)
+  # Return a matrix that is the inverse of 'x'
   iom
 }
+
+
+
